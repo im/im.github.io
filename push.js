41e5bba33692a4fd77446d0180ea1f8dd3f07bd9
im <81195314@qq.com>
2021-05-26 15:38:35 +0800

    notes update 2021-05-We 03:38:35

--- push.js
+++ push.js
@@ -1,11 +1,12 @@
 const simpleGit = require('simple-git')
+const moment = require('moment')
 require('./deletePersonal.js')
 require('./getNotes.js')
 
 setTimeout(() => {
     simpleGit()
     .add('./*')
-    .commit('notes updates')
+    .commit(`notes update ${ moment().format('yyyy-MM-dd hh:mm:ss') }`)
     .push(['-u', 'origin', 'main'], (e) => {
         // console.log('e: ', e);
     })
