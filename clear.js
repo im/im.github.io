55fa170cbc3bebccca4b01012a7acbfca74faffe
im <81195314@qq.com>
2021-05-28 09:23:18 +0800

    notes update 2021-05-28 09:23:18

--- clear.js
+++ clear.js
@@ -1,29 +1,31 @@
-const fs = require('fs')
-const path = require('path')
-const bearPath = path.join(__dirname, 'bear')
+module.exports = (() => {
+    const fs = require('fs')
+    const path = require('path')
+    const bearPath = path.join(__dirname, 'bear')
 
-function deleteFolder(path) {
-    let files = []
-    if (fs.existsSync(path) && fs.statSync(path).isDirectory()) {
-        files = fs.readdirSync(path)
-        files.forEach(function (file, index) {
-            let curPath = path + '/' + file
-            if (fs.statSync(curPath).isDirectory()) {
-                deleteFolder(curPath)
-            } else {
-                fs.unlinkSync(curPath)
-            }
-        })
-        fs.rmdirSync(path)
-    } else {
-        fs.unlinkSync(path)
+    function deleteFolder(path) {
+        let files = []
+        if (fs.existsSync(path) && fs.statSync(path).isDirectory()) {
+            files = fs.readdirSync(path)
+            files.forEach(function (file, index) {
+                let curPath = path + '/' + file
+                if (fs.statSync(curPath).isDirectory()) {
+                    deleteFolder(curPath)
+                } else {
+                    fs.unlinkSync(curPath)
+                }
+            })
+            fs.rmdirSync(path)
+        } else {
+            fs.unlinkSync(path)
+        }
     }
-}
 
-if (fs.existsSync(bearPath)) {
-    let files = fs.readdirSync(bearPath)
-    files.forEach(function (file, index) {
-        let curPath = bearPath + '/' + file
-        deleteFolder(curPath)
-    })
-}
+    if (fs.existsSync(bearPath)) {
+        let files = fs.readdirSync(bearPath)
+        files.forEach(function (file, index) {
+            let curPath = bearPath + '/' + file
+            deleteFolder(curPath)
+        })
+    }
+})()
--- push.js
+++ push.js
@@ -2,12 +2,13 @@ const simpleGit = require('simple-git')
 const moment = require('moment')
 require('./deletePersonal.js')
 require('./getNotes.js')
+require('./clear.js')
 
 setTimeout(() => {
     simpleGit()
     .add('./*')
     .commit(`notes update ${ moment().format('yyyy-MM-DD HH:mm:ss') }`)
     .push(['-u', 'origin', 'main'], (e) => {
-        // console.log('e: ', e);
+        require('./clear.js')
     })
 }, 3000)
